05f261d73d0471877e4e196a3d970f6b640937d5
Jonathan Speek <jonspeek@gmail.com>
2016-01-30 16:02:58 -0700

    Loop runs on Runs#Index

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -1,7 +1,7 @@
 class RunsController < ApplicationController
   before_action :find_run, only: [:show, :edit, :update, :destroy]
   def index
-
+    @runs = Run.all.order('created_at DESC')
   end
 
   def show

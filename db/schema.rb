6fde54f0205e59ba0fa65d0d666e731fe34dc9a2
Jonathan Speek <jonspeek@gmail.com>
2016-08-14 14:55:28 -0600

    Added uid to Users via new migration to use for OmniAuth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812021424) do
+ActiveRecord::Schema.define(version: 20160814205122) do
 
   create_table "runs", force: :cascade do |t|
     t.datetime "date"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20160812021424) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

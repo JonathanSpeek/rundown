f2930fcdb3249cba9893a071a23db15ada9807ce
Jonathan Speek <jonspeek@gmail.com>
2016-08-13 19:07:19 -0600

    Fixed dev env issues by using SendGrid instead of GMAil

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,19 +15,30 @@ Rails.application.configure do
 
     # Don't care if the mailer can't send.
     config.action_mailer.raise_delivery_errors = true
-    config.action_mailer.default_url_options = { host: 'localhost:3000' }
+    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
     config.action_mailer.delivery_method = :smtp
 
-    config.action_mailer.smtp_settings = {
-        address: "smtp.gmail.com",
-        port: 587,
-        domain: ENV["GMAIL_DOMAIN"],
-        authentication: "plain",
-        enable_starttls_auto: true,
-        user_name: ENV["GMAIL_USERNAME"],
-        password: ENV["GMAIL_PASSWORD"]
+    # config.action_mailer.smtp_settings = {
+    #     address: "smtp.gmail.com",
+    #     port: 587,
+    #     domain: ENV["GMAIL_DOMAIN"],
+    #     authentication: "plain",
+    #     enable_starttls_auto: true,
+    #     user_name: ENV["GMAIL_USERNAME"],
+    #     password: ENV["GMAIL_PASSWORD"]
+    # }
+
+    ActionMailer::Base.smtp_settings = {
+      :address        => 'smtp.sendgrid.net',
+      :port           => '587',
+      :authentication => :plain,
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
+      :domain         => 'heroku.com',
+      :enable_starttls_auto => true
     }
+
     # Print deprecation notices to the Rails logger.
     config.active_support.deprecation = :log
 

1ee9d74412561242fa1fcffae8004d42810a8e28
Jonathan Speek <jonspeek@gmail.com>
2016-08-13 18:13:51 -0600

    added sign-up confirmation mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ActiveRecord::Base
    has_many :runs
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 end

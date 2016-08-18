ee773b739c8e9efbd3daf2c70fbdf2b8cd78404c
Jonathan Speek <jonspeek@gmail.com>
2016-08-17 22:52:36 -0600

    changed format.html in redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
       if @user.update(user_params)
         @user.skip_reconfirmation!
         sign_in(@user, :bypass => true)
-        redirect_to @user, notice: 'Your profile was successfully updated.'
+        format.html { redirect_to @user, notice: 'Your profile was successfully updated.' }
       else
         @show_errors = true
       end
